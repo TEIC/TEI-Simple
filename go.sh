94885f88878c104c331e8e8209dd5ae2a42e9e20
Lou <lou.burnard@retired.ox.ac.uk>
2015-05-10 14:02:44 +0100

    tidying

--- go.sh
+++
@@ -1,3 +0,0 @@
-saxon -o:allota.odd -it:main ~/TEI/Stylesheets/tools/oddbyexample.xsl corpus=/Volumes/Repo/Simple/ota enumerateRend=true enumerateType=true verbose=true
-saxon -o:alldta.odd -it:main ~/TEI/Stylesheets/tools/oddbyexample.xsl corpus=/Volumes/Repo/Simple/dta enumerateRend=true enumerateType=true verbose=true
-
